c47a6f02c423b93cc25bbee115ed13194b4f4244
nasrcristian <nasrcristian.un@gmail.com>
2026-09-21 23:50:05 -0300

    service mock

--- backend/internal/service/player_service.go
+++ backend/internal/service/player_service.go
@@ -1,8 +1,15 @@
 package service
 
+import "github.com/nicotorboli/2026s2-desapp-NoSePudo/backend/internal/model"
 
-type  
+type PlayerRepository interface {
+	GetPlayers() []model.Player
+}
 
 type PlayerService struct {
+	playerRepo PlayerRepository
+}
 
+func (p *PlayerService) ListPlayers() []model.Player {
+	return p.playerRepo.GetPlayers()
 }
